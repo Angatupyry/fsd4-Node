d65d06cdc09e0cd5450f78775dc30ab86963c062
Angatupyry <fierrofenix@gmail.com>
2021-10-07 19:21:11 -0300

    Clase 1

---
+++ Clase2/Ejercicio1.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+// const data = fs.readFileSync("text.txt");
+
+// console.log(data.toString());
+// console.log("Programa terminado");
+
+fs.readFile("te.txt", function name(error, data) {
+  if (error) return console.log(error);
+
+  console.log(data.toString());
+});
+
+console.log("Programa terminado");
