6f296a5ec40a72f427b9e09597541cf7fb099fec
Angatupyry <fierrofenix@gmail.com>
2021-11-04 20:35:20 -0300

    Se agrega clase 10

---
+++ Clase10/middleware/jw-validate.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+
+const TOKEN_SECRET = "UnaClaveParaFirmarelToken";
+
+const verifyToken = (req, res, next) => {
+  try {
+    const token = req.header("auth-token");
+
+    if (!token) {
+      return res.status(403).json({ error: "Acceso denegado" });
+    }
+
+    const verified = jwt.verify(token, TOKEN_SECRET);
+
+    req.user = verified;
+    next();
+  } catch (error) {
+    return next(error);
+  }
+};
+
+module.exports = {
+  TOKEN_SECRET,
+  verifyToken,
+};
--- Clase10/routes/auth.js
+++ Clase10/routes/auth.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
-
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+
+const { TOKEN_SECRET, verifyToken } = require("../middleware/jw-validate");
 
 router.get("/", (req, res) => {
   res.send("hola desde auth");
@@ -52,7 +54,46 @@ router.post("/registro", async (req, res, next) => {
   }
 });
 
-router.get("/usuarios", (req, res) => {
+router.post("/login", async (req, res, next) => {
+  try {
+    const user = usuarios.find((u) => u.mail === req.body.mail);
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Usuario no encontrado" });
+    }
+
+    const contrasenhaValidada = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!contrasenhaValidada) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Password inválido" });
+    }
+
+    const token = jwt.sign(
+      {
+        name: user.name,
+        mail: user.mail,
+      },
+      TOKEN_SECRET
+    );
+
+    return res.status(200).json({
+      success: true,
+      data: user,
+      token: token,
+    });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+router.get("/usuarios", verifyToken, (req, res) => {
   return res.send({ error: null, usuarios });
 });
 
